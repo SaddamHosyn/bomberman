d7e5315f3a22c5ba48e06079f4d40bcc4e9f48e6
sfasasa <ejmilli03@gmail.com>
2025-07-25 16:15:41 +0300

    made structs for the websockets part

--- backend/models/sockets.go
+++ backend/models/sockets.go
@@ -46,30 +46,6 @@ type Hub struct {
 	Broadcast  chan *WebSocketMessage
 }
 
-const (
-	// Lobby related messages
-	MSG_JOIN_LOBBY    = "join_lobby"
-	MSG_LEAVE_LOBBY   = "leave_lobby"
-	MSG_LOBBY_UPDATE  = "lobby_update"
-	MSG_PLAYER_JOINED = "player_joined"
-	MSG_PLAYER_LEFT   = "player_left"
-
-	// Chat related messages
-	MSG_CHAT_MESSAGE = "chat_message"
-	MSG_CHAT_HISTORY = "chat_history"
-
-	// Game related messages
-	MSG_GAME_START  = "game_start"
-	MSG_GAME_UPDATE = "game_update"
-	MSG_GAME_END    = "game_end"
-
-	// System messages
-	MSG_ERROR   = "error"
-	MSG_SUCCESS = "success"
-	MSG_PING    = "ping"
-	MSG_PONG    = "pong"
-)
-
 type ErrorResponse struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
